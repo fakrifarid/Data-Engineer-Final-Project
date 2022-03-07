b2fa039f6731c6999ead07dc234e682c763ca869
Fakri Farid <95616496+fakrifarid@users.noreply.github.com>
2022-03-07 23:43:59 +0700

    Update final_project_spark.py

--- airflow-docker/spark/final_project_spark.py
+++ airflow-docker/spark/final_project_spark.py
@@ -8,29 +8,36 @@ spark = SparkSession \
 
 
 #convert and write csv to parquet
-df_temp = spark.read.options(header='true', inferSchema='true').csv("gs://data_final_project/INPUT/GlobalLandTemperaturesByCity.csv")
-df_temp.write.mode("Overwrite").parquet("gs://data_final_project/raw/globaltempbycity.parquet")
+df_temp = spark.read.options(header='true', inferSchema='true').csv("gs://file_final_project/Input/GlobalLandTemperaturesByCity.csv")
+df_temp.write.mode("Overwrite").parquet("gs://file_final_project/raw/globaltempbycity.parquet")
 
-df_airport = spark.read.options(header='true', inferSchema='true').csv("gs://data_final_project/INPUT/airport-codes_csv.csv")
-df_airport.write.mode("Overwrite").parquet("gs://data_final_project/raw/airportcodes.parquet")
+df_airport = spark.read.options(header='true', inferSchema='true').csv("gs://file_final_project/Input/airport-codes_csv.csv")
+df_airport.write.mode("Overwrite").parquet("gs://file_final_project/raw/airportcodes.parquet")
 
-df_imigration = spark.read.options(header='true', inferSchema='true').csv("gs://data_final_project/INPUT/immigration_data_sample.csv")
-df_imigration.write.mode("Overwrite").parquet("gs://data_final_project/raw/imigration.parquet")
+df_immigration = spark.read.options(header='true', inferSchema='true').csv("gs://file_final_project/Input/immigration_data_sample.csv")
+df_immigration.write.mode("Overwrite").parquet("gs://file_final_project/raw/immigration.parquet")
 
-df_demographic = spark.read.options(header='true', inferSchema='true').csv("gs://data_final_project/INPUT/us-cities-demographics.csv")
-df_demographic.write.mode("Overwrite").parquet("gs://data_final_project/raw/demographic.parquet")
+df_port = spark.read.options(header='true', inferSchema='true').csv("gs://file_final_project/Input/USPORT.csv")
+df_port.write.mode("Overwrite").parquet("gs://file_final_project/raw/usport.parquet")
 
-df_port = spark.read.options(header='true', inferSchema='true').csv("gs://data_final_project/INPUT/USPORT.csv")
-df_port.write.mode("Overwrite").parquet("gs://data_final_project/raw/usport.parquet")
+df_state = spark.read.options(header='true', inferSchema='true').csv("gs://file_final_project/Input/USSTATE.csv")
+df_state.write.mode("Overwrite").parquet("gs://file_final_project/raw/usstate.parquet")
 
-df_state = spark.read.options(header='true', inferSchema='true').csv("gs://data_final_project/INPUT/USSTATE.csv")
-df_state.write.mode("Overwrite").parquet("gs://data_final_project/raw/usstate.parquet")
+df_country = spark.read.options(header='true', inferSchema='true').csv("gs://file_final_project/Input/USCOUNTRY.csv")
+df_country.write.mode("Overwrite").parquet("gs://file_final_project/raw/uscountry.parquet")
 
-df_country = spark.read.options(header='true', inferSchema='true').csv("gs://data_final_project/INPUT/USCOUNTRY.csv")
-df_country.write.mode("Overwrite").parquet("gs://data_final_project/raw/uscountry.parquet")
+# The delimiter for this file (us-cities-demographics.csv) is semicolon (;)
+# We have to change the default delimiter
+df_demographic = spark.read.options(header="true", inferSchema="true", delimiter=";").csv("gs://file_final_project/Input/us-cities-demographics.csv")
+df_demographic.printSchema()
+# There is a problem in Header of this df, 
+# Therefore, we need to rename the header before write it into parquet file
+# Header: City;State;Median Age;Male Population;Female Population;Total Population;Number of Veterans;Foreign-born;Average Household Size;State Code;Race;Count
 
+# Rename all column names with new column names
+demographic_new_col_names = ["City", "State", "MedianAge", "MalePopulation", "FemalePopulation", "TotalPopulation", "NumberOfVeterans", "ForeignBorn", "AverageHouseholdSize", "StateCode", "Race", "Count"]
+df_demographic = df_demographic.toDF(*demographic_new_col_names)
 
-#agregare 
-#df.createOrReplaceTempView("sales")
-#highestPriceUnitDF = spark.sql("select * from sales where UnitPrice >= 3.0")
-#highestPriceUnitDF.write.mode("Overwrite").parquet("gs://hive-example/final-project/output/highest_prices.parquet")
+# Then, we convert it into parquet
+df_demographic.write.mode("Overwrite").parquet("gs://file_final_project/raw/demographic.parquet")
+
